ea6891eaf7dab57bcb5cd7e3a3c79b7995387bcf
gopalkhanal <smegopal@hotmail.com>
2018-02-08 15:04:13 -0800

    my project set up completed.

---
+++ shopping-backend/src/main/java/com/fullstack/shopping_backend/App.java
@@ -0,0 +1,13 @@
+package com.fullstack.shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
